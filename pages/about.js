402ade29fcb0f6e4487b8b5f3bcef9380276ace8
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2020-06-21 12:55:14 +0430

    Simple routes added

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const About = () => (
+  <>
+    <Head>
+      <title>About Next Playground</title>
+    </Head>
+    <div>
+      <h2>A place for playing with Next JS! :D</h2>
+    </div>
+  </>
+);
+
+export default About;
